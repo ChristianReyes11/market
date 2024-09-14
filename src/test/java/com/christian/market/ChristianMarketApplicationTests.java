2aafdaab7fe27bc7e8f97257a6effffdf69e1d60
ChristianReyes11 <christianamaro2000@gmail.com>
2024-09-13 22:42:10 -0600

    init project

---
+++ src/test/java/com/christian/market/ChristianMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristianMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
